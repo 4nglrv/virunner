d7e4b55601792ef393dde954dc364c9851d4d326
hakuu1251 <hakuu1251@gmail.com>
2020-09-06 19:36:11 +0300

    second commit

---
+++ src/hoc/layout/Layout.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+import Main from "../../containers/analysis/Main";
+
+class Layout extends Component {
+    render() {
+        return(
+            <div>
+                <main>
+                    <Main />
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Layout
