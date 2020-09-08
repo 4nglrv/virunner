d4e3819059b4da367d17674821786b839bcc4b26
hakuu1251 <hakuu1251@gmail.com>
2020-09-08 17:11:28 +0300

    ContentLinks

--- src/hoc/layout/Layout.js
+++ src/hoc/layout/Layout.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
 import classes from "./Layout.module.css"
 import Button from "../../components/UI/button/Button";
+import ContentNavLink from "../../components/UI/navLink/ContentNavLink";
 
 
 class Layout extends Component {
@@ -19,12 +20,24 @@ class Layout extends Component {
                                 <div className={classes.desc}>dynamic file analysis</div>
                             </div>
                         </div>
-                        <div className={classes.reg}>
-                            <Button type="primary">Sign in</Button>
-                            <Button type="secondary">Sign up</Button>
+                        <div className={classes.right}>
+                            <div className={classes.reg}>
+                                <Button type="primary">Sign in</Button>
+                                <Button type="secondary">Sign up</Button>
+                            </div>
+                            <div className={classes.circle}>
+                                <div className={classes.burger}/>
+                            </div>
                         </div>
                     </nav>
-                    {this.props.children}
+                    <div className={classes.content}>
+                        <div className={classes.leftContent}>
+                            <ContentNavLink link="/analysis" active="false" icon="analysis" />
+                        </div>
+                        <div className={classes.rightContent}>
+                            {this.props.children}
+                        </div>
+                    </div>
                 </main>
             </div>
         )
